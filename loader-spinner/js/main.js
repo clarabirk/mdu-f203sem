2dce0864e0203bd995926792ac07cfe9fce0ec7d
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-02-26 17:08:40 +0100

    Update main.js

--- loader-spinner/js/main.js
+++ loader-spinner/js/main.js
@@ -2,10 +2,19 @@
 
 /* ----------- The DOM is ready ----------- */
 document.addEventListener("DOMContentLoaded", function () {
+  // initSpinner();
   setTimeout(function () {
     showLoader(false); // hide loader after 3 sec
   }, 3000)
 });
+
+// function initSpinner() {
+//   document.querySelector("body").innerHTML += /*html*/ `
+//     <div id="loader">
+//       <div class="spinner"></div>
+//     </div>
+//   `;
+// }
 
 // to shoe and hide the loader
 function showLoader(show) {
